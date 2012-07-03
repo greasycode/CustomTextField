2d931af04dbc6e32c01be17c09224644cefc2d86
gonecrazy <gonecrazy@gonecrazy-Vostro-1550.(none)>
2012-07-03 12:32:08 +0530

    changed models.py

--- textfield/textfield/rboxtextfield/custom_filefield/field/models.py
+++ textfield/textfield/rboxtextfield/custom_filefield/field/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from rboxfilefield import RboxFileField
 from storage_backends.couchfs import CouchFSStorage
+from storage_backends.combinedfs import CombinedFSStorage
 from storage_backends.s3botofs import S3BotoStorage
 class Doc(models.Model):
     name = models.CharField('Name', max_length="100")
-    doc = RboxFileField('File', max_length="2", backup_storage=S3BotoStorage(zip_n_save=True), upload_to="plaban")
+    doc = RboxFileField('File', max_length="2", primary_storage=CombinedFSStorage(), upload_to="plaban")
